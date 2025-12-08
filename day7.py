417f990d1084ad8a21f812de93321ea3e63b1f8d
Mark Baker <mrbaker1917>
2025-12-07 17:28:56 -0800

    day7 part 2 donegit status!

--- day7.py
+++ day7.py
@@ -1,6 +1,6 @@
 data = []
 
-with open("day7_test_data.txt", "r") as f:
+with open("day7_data.txt", "r") as f:
     data = [l.strip() for l in f.readlines()]
 
 data = [[c for c in l] for l in data]
@@ -27,8 +27,8 @@ while i < len(data)-1:
 if data[-1][0] != "|" and data[-2][0] == "|":
     data[-1][0] = "|"
 
-for r in data:
-    print(r)
+# for r in data:
+#     print("".join(r))
 splits = 0
 for i, r in enumerate(data):
     for k, c in enumerate(r):
@@ -38,9 +38,24 @@ for i, r in enumerate(data):
 # print(splits)
 
 # part 2:
-paths = 0
-for i, r in enumerate(data):
-    for j, c in enumerate(r):
-        if c == "|":
-            paths += 1
-print(paths)  # +1 for the starting point
+# paths = 0
+# for i, r in enumerate(data):
+#     for j, c in enumerate(r):
+#         if c == "|":
+#             paths += 1
+# print(paths)
+
+for i in range(len(data)-1, 0, -1):
+    for j in range(len(data[i])):
+        # print(i, j)
+        if i == len(data)-1:
+            if data[i][j] == "|":
+                data[i][j] = "1"
+        if data[i][j] == "|":
+            if data[i-1][j] in ["|", "."] and data[i+1][j] != "^":
+                data[i][j] = data[i+1][j]
+            if data[i+1][j] == "^":
+                data[i][j] = str(int(data[i+1][j-1])+int(data[i+1][j+1]))
+
+print(data[1][entry])
+
