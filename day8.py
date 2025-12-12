962efd9fe9a7a52127dadff4006109d15ceb3dd9
Mark R. Baker <mrbaker1917@gmail.com>
2025-12-11 17:47:07 -0800

    completes day 8 part 1

--- day8.py
+++ day8.py
@@ -1,5 +1,5 @@
 import math
-
+from collections import defaultdict
 data = []
 
 with open("day8_data.txt", "r") as f:
@@ -8,72 +8,41 @@ with open("day8_data.txt", "r") as f:
 data = [l.split(",") for l in data]
 
 data = [[int(n) for n in l] for l in data]
-
-# print(data)
+data = [(l[0], l[1], l[2]) for l in data]
 
 def closeness(p1, p2):
     return round(math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 + (p1[2]-p2[2])**2), 2)
 
-d = {}
+D = []
+for i, (x1, y1,z1) in enumerate(data):
+    for j, (x2, y2, z2) in enumerate(data):
+        distance = closeness((x1,y1,z1), (x2,y2,z2))
+        if i>j:
+            D.append((distance, i, j))
 
-for j in range(len(data)):
-    p1 = data[j]
-    l = data[:j] + data[j+1:]
-    for p in l:
-        dist = closeness(p1, p)
-        d[dist] = [p1, p]
+uf = {i: i for i in range(len(data))}
 
-d_keys = sorted(d.keys())
-# for k in d_keys[:10]:
-#     print(k, d[k])
+def find(x):
+    if x == uf[x]:
+        return x
+    uf[x] = find(uf[x])
+    return uf[x]
 
-circuit_pairs = []
+def mix(x, y):
+    uf[find(x)] = find(y)
 
-for k in d_keys:
-    p1, p2 = d[k]
-    p1s = "-".join(map(str, p1))
-    p2s = "-".join(map(str, p2))
-    circuit_pairs.append([p1s, p2s])
-
-# print(circuit_pairs)
 connections = 0
-circuits = []
-i = 0
-while connections < 2000:
-    added = False
-    pair = circuit_pairs[i]
-    for j in range(len(circuits)):
-        if pair[0] in circuits[j] and pair[1] in circuits[j]:
-            added = True
-            circuit_pairs[i] = []
-            connections += 1
-            break
-        elif pair[0] in circuits[j]:
-            circuits[j].append(pair[1])
-            added = True
-            circuit_pairs[i] = []
-            connections += 1
-            break
-            
-        elif pair[1] in circuits[j]:
-            circuits[j].append(pair[0])
-            added = True
-            circuit_pairs[i] = []
-            connections += 1
-            break
-    
-    if not added:   
-        circuits.append(pair)
+D = sorted(D)
+for distance, i, j in D[:1000]:
+    if find(i) != find(j):
         connections += 1
-    i += 1
-    # print(f"connections: {connections}")
+        if connections == len(data) - 1:
+            print(data[i][0]*data[j][0])
+        mix(i,j)
 
-circuits = sorted(circuits, key=len, reverse=True)
-print(len(circuits))
-# for ci in circuits[:10]:
-    # print(ci, len(ci))
-total_circ = 1
-for c in circuits[:3]:
-    print(c, len(c))
-    total_circ *= len(c)
-print(total_circ)
+sz = defaultdict(int)
+
+for x in range(len(data)):
+    sz[find(x)] += 1
+s = sorted(sz.values())
+print(s[-1]*s[-2]*s[-3])
